c20f9ed19057075e54318bca6e93d26c3b6564a2
Daniel Halligan <dan.halligan@fiosgenomics.com>
2020-12-10 10:02:20 +0000

    Adds q10

---
+++ q10/q10.R
@@ -0,0 +1,12 @@
+options(scipen=999)
+
+x <- read.table("input.txt")[[1]]
+x <- sort(c(0, x, max(x) + 3))
+
+# part a
+prod(rle(sort(diff(x)))$lengths)
+
+# part b
+r <- rle(diff(x)))
+olr <- rle(sort(r$lengths[r$values == 1]))
+prod(c(1, 2, 4, 7)[olr$values] ^ olr$lengths)
