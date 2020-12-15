1f1977e884bc9b0b8cab5766bfc6ca56afbdbc52
Dan Halligan <dan.halligan@gmail.com>
2020-12-15 21:47:21 +0000

    Neater q15

--- q15/q15.rb
+++ q15/q15.rb
@@ -1,17 +1,14 @@
 def iterate(start, n)
-  lastpos = Hash.new{ |h,k| h[k] = [] }
-  start.each_with_index {|x, v| lastpos[x.to_s] = [v + 1] }
+  h = Hash.new{ |h,k| h[k] = [] }
+  start.each_with_index {|x, v| h[x] = [v + 1] }
   last = start[-1]
-  s = start.length + 1
-  (s..n).each do |i|
-    pos = lastpos[last.to_s].nil? ? 0 : lastpos[last.to_s]
-    last = pos.length <= 1 ? 0 : pos[1] - pos[0]
-    lastpos[last.to_s] << i
-    lastpos[last.to_s] = lastpos[last.to_s].pop(2)
+  ((start.length + 1)..n).each do |i|
+    last = h[last].length <= 1 ? 0 : h[last][-1] - h[last][-2]
+    h[last] << i
   end
   last
 end
 
-iterate([0, 3, 6], 12)
-iterate([2, 15, 0, 9, 1, 20], 2020)
-iterate([2, 15, 0, 9, 1, 20], 30000000)
+puts iterate([0, 3, 6], 12)
+puts iterate([2, 15, 0, 9, 1, 20], 2020)
+puts iterate([2, 15, 0, 9, 1, 20], 30000000)
