67d066241a249e8195ef2eb99f7744d62ccc6417
Daniel Halligan <dan.halligan@fiosgenomics.com>
2020-12-18 18:15:25 +0000

    Adds q17 / q18

---
+++ q18/q18.rb
@@ -0,0 +1,32 @@
+def eval_block1(s)
+  while s.match? /\s/
+    s.sub!(/^\d+\s.\s\d+/) {|e| eval(e) }
+  end
+  s.to_i
+end
+
+def eval_exprs(x)
+  while x.match? /\(/
+    x.sub!(/\(([^\(]+?)\)/) { |m| yield $1 }
+  end
+  yield x
+end
+
+def eval_block2(s)
+  while s.match? /\+/
+    s.sub!(/\d+\s\+\s\d+/) {|e| eval(e) }
+  end
+  eval_block1(s)
+end
+
+# part a
+sums = IO.readlines('input.txt', chomp: true).map do |line|
+  eval_exprs(line) { |x| eval_block1(x) }
+end
+sums.reduce(&:+)
+
+# part b
+sums = IO.readlines('input.txt', chomp: true).map do |line|
+  eval_exprs(line) { |x| eval_block2(x) }
+end
+sums.reduce(&:+)
