9b2f30d45734b9cb96e916fc34555eec36f6dfcc
Daniel Halligan <dan.halligan@fiosgenomics.com>
2020-12-18 19:02:38 +0000

    Formatting

--- q18/q18.rb
+++ q18/q18.rb
@@ -1,20 +1,20 @@
 def eval_block1(s)
   while s.match? /\s/
-    s.sub!(/^\d+\s.\s\d+/) {|e| eval(e) }
+    s.sub!(/^\d+\s.\s\d+/) { |e| eval(e) }
   end
   s.to_i
 end
 
 def eval_exprs(x)
   while x.match? /\(/
-    x.sub!(/\(([^\(]+?)\)/) { |m| yield $1 }
+    x.gsub!(/\(([^\(]+?)\)/) { |m| yield $1 }
   end
   yield x
 end
 
 def eval_block2(s)
   while s.match? /\+/
-    s.sub!(/\d+\s\+\s\d+/) {|e| eval(e) }
+    s.gsub!(/\d+\s\+\s\d+/) { |e| eval(e) }
   end
   eval_block1(s)
 end
@@ -23,10 +23,10 @@ end
 sums = IO.readlines('input.txt', chomp: true).map do |line|
   eval_exprs(line) { |x| eval_block1(x) }
 end
-sums.reduce(&:+)
+puts sums.reduce(&:+)
 
 # part b
 sums = IO.readlines('input.txt', chomp: true).map do |line|
   eval_exprs(line) { |x| eval_block2(x) }
 end
-sums.reduce(&:+)
+puts sums.reduce(&:+)
