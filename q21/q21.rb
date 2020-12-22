09d15ca18320fa5459338010eb47b2e81dcd4d80
Dan Halligan <dan.halligan@gmail.com>
2020-12-22 08:11:51 +0000

    Tidying

--- q21/q21.rb
+++ q21/q21.rb
@@ -1,37 +1,30 @@
 # Read data
 foods = IO.readlines("input.txt", chomp: true).map do |line|
-  line = line.sub(/\s*\(contains\s(.+)\)/, '')
-  contains = $1.split(', ')
-  ingredients = line.split(' ')
-  {i: ingredients, a: contains}
+  line =~ /(.+)\s\(contains\s(.+)\)/
+  {i: $1.split(' '), a: $2.split(', ')}
 end
 
-# build map of allergens to possible ingredients
+# If a food has an allergen, it must contain the ingredient with that allergen
+# We can get possible list of ingredients for each allergen by taking the
+# intersection of ingredients across foods with that allergen.
 allergens = foods.map { |x| x[:a] }.flatten.uniq.sort
-ahash = allergens.map do |a|
-  ing = foods.select {|f| f[:a].include? a }.map { |f| f[:i] }.reduce(&:&)
-  [a, ing]
-end.to_h
-
-# find cases where there's only one possibility, drop that option from others
-# and continue till we have assigned everything.
-found = {}
-while ahash.length > 0
-   singles = ahash.select {|k,v| v.length == 1 }
-   found = found.merge(singles)
-   sk = singles.keys
-   sv = singles.values.flatten
-   ahash = ahash.map {|k,v| [k, v - sv] }.to_h
-   ahash = ahash.select {|k, v| !sk.include? k }
+assigned = allergens.to_h do |a|
+  ing = foods.select {|f| f[:a].include? a }
+  [a, ing.map { |f| f[:i] }.reduce(&:&)]
 end
 
 # part a
-ingredients = foods.map { |x| x[:i] }.flatten.uniq.sort
-invalid = ingredients - found.map{|k,v| v}.flatten
-ingred = foods.map { |x| x[:i] }.flatten
-are_invalid = ingred.map { |i| invalid.include?(i) }
+ingredients = foods.map { |x| x[:i] }.flatten
+invalid = ingredients - assigned.map{|k,v| v}.flatten
+are_invalid = ingredients.map { |i| invalid.include?(i) }
 puts are_invalid.count(true)
 
+# Since an allergen can only be caused by one ingredient, we can prune this list.
+while assigned.values.map(&:length).max > 1
+  known = assigned.select {|k,v| v.length == 1 }.values.flatten
+  assigned = assigned.to_h { |k,v| [k,  v.length > 1 ? v - known : v] }
+end
+
 # part b
-puts found.keys.sort.map {|x| found[x]}.join(',')
+puts assigned.keys.sort.map {|x| assigned[x]}.join(',')
 
