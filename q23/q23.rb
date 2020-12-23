e439511ffc3bb88dfa95a2bfbf0f7a601f7f8110
Dan Halligan <dan.halligan@gmail.com>
2020-12-23 15:20:45 +0000

    TIdy

--- q23/q23.rb
+++ q23/q23.rb
@@ -1,58 +1,13 @@
-load './circularlist.rb'
+load './crabcups.rb'
 
-class CrabCups
-  attr_accessor :x
+# test
+test = '389125467'.split('').map(&:to_i)
+CrabCups.new(test).play.result_a == '92658374'
+CrabCups.new(test).play(100).result_a == '67384529'
 
-  def initialize(input)
-    @x = CircularList.new
-    input.split('').each { |v| @x.insert(v.to_i) }
-    @max = @x.max
-  end
+# part a
+dat = '685974213'.split('').map(&:to_i)
+puts CrabCups.new(dat).play(100).result_a   # 82635947
 
-  def move
-    v = @x.head.data
-    hold = @x.remove_n 3
-    target = v
-    loop do
-      target -= 1
-      target = @max if target < 1
-      break unless hold.include? target
-    end
-    @x.insert_next @x.find_first {|e| e.data == target}, hold
-    @x.head = @x.head.next
-  end
-
-  def play(n = 10)
-    n.times do
-      move
-    end
-    @x.head = @x.find_first {|e| e.data == 1}
-    @x.to_a.drop(1).join('')
-  end
-
-  def playb(n = 10000000)
-    l = @x.to_a.last
-    add = (@x.max + 1)..(1e6)
-    @x.insert_next @x.find_first {|e| e.data == l}, add
-    @max = 1e6
-    i = 0
-    n.times do
-      i += 1
-      print '.'
-      move
-    end
-    @x
-  end
-
-end
-
-
-CrabCups.new('389125467').play(100) # 67384529
-CrabCups.new('685974213').play(100) # 82635947
-
-r = CrabCups.new('685974213').playb(10000000)
-
-r = playb('685974213', 10000000)
-x.head = x.find_first {|e| e.data == 1}
-x.to_a.drop(1).join('')
-
+# part b
+puts  CrabCups.new(dat + (10..1e6).to_a).play(10000000).result_b
